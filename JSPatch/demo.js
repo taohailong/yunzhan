3637d3bc9c2758540a792cd7f81c9bed4b0e24c6
taohailong <taohailong1988@gmail.com>
2016-02-22 11:45:22 +0800

    over

--- JSPatch/demo.js
+++ JSPatch/demo.js
@@ -13,16 +13,21 @@ defineClass('yunzhan.AboutVC', {
            var image = temp;
            image.setImage(UIImage.imageNamed("aboutImage"));
         }
-
-//        for (var i = 0; i < self.view().subviews().count(); i++)
-//        {
-//            var temp = self.view().subviews().objectAtIndex(i);
-//            if (temp.isKindOfClass(UIImageView.class()))
-//            {
-//              var image = temp;
-//              image.setImage(UIImage.imageNamed("aboutImage"));
-//            }
-//        }
-    
     }
+});
+
+defineClass('yunzhan_external.AboutVC', {
+            
+            viewWillAppear: function(animated) {
+            
+            require('UIImage,UIImageView')
+            self.super().viewWillAppear(animated);
+            
+            var temp = self.view().subviews().objectAtIndex(0);
+            if (temp.isKindOfClass(UIImageView.class()))
+            {
+            var image = temp;
+            image.setImage(UIImage.imageNamed("aboutImage"));
+            }
+            }
 });
